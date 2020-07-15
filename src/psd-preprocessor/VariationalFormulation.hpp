81581009cac017414fa68203f0ebe98ffa88061c
MAB <afeef.badri@gmail.com>
2020-07-15 15:05:27 +0200

    add header

--- src/psd-preprocessor/VariationalFormulation.hpp
+++ src/psd-preprocessor/VariationalFormulation.hpp
@@ -1,6 +1,17 @@
-//=================================================================================================
-// ------ Building the VariationalFormulations.edp file ------ 
-//=================================================================================================
+/**************************************************************************************
+*                                                                                     *
+* Author : Mohd Afeef BADRI                                                           *
+* Email  : mohd-afeef.badri@cea.fr                                                    *
+* Date   : 20/04/2020                                                                 *
+* Type   : Support file                                                               *
+*                                                                                     *
+* Comment: This support file is responsible for generating VariationalFormulations.edp*
+*          the file which contains FEM variational  formulation  for  PSD. Infact this*
+*          variational formulations for specific applications ( damage, elastodynamics*
+*          linear elasticity, soildynamics ) are written in other files and  this file*
+*          helps choose the correct one                                               *
+*                                                                                     *
+**************************************************************************************/
 
 cout << " building VariationalFormulations.edp"; 
 
