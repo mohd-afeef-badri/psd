0bfe60271cc0f7dceba5672b89f6af042cf09922
Afeef <afeef.badri@gmail.com>
2021-12-06 15:51:54 +0100

    exclude gmsh folder

--- etc/PSD-lines.sh
+++ etc/PSD-lines.sh
@@ -1,2 +1,9 @@
-cd ..; 
-find * -regextype posix-extended -regex '.*\.(edp|cpp|h|hpp|c|geo|am|ac|MD|script)' | xargs wc -l
+cd ..;
+echo "---------------------------------------------------------------"
+echo "TOTAL"
+echo "---------------------------------------------------------------"
+find * -regextype posix-extended -regex '.*\.(edp|cpp|h|hpp|c|geo|am|ac|MD|script|mfront)' | xargs wc -l
+echo "---------------------------------------------------------------"
+echo "MESHING"
+echo "---------------------------------------------------------------"
+find * -regextype posix-extended -regex '.*\.(geo)' | xargs wc -l
