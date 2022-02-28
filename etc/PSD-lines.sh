f4d476a91fb48df47c618a5131219cbfceae2b61
Afeef <afeef.badri@gmail.com>
2022-02-28 10:35:34 +0100

    Mr. Clean

--- etc/PSD-lines.sh
+++ etc/PSD-lines.sh
@@ -2,8 +2,8 @@ cd ..;
 echo "---------------------------------------------------------------"
 echo "TOTAL"
 echo "---------------------------------------------------------------"
-find * -regextype posix-extended -regex '.*\.(edp|cpp|h|hpp|c|geo|am|ac|MD|script|mfront)' | xargs wc -l
+git ls-files --exclude ./src/* ./demos/* ./data/* ./test/* ./etc/* | xargs wc -l
 echo "---------------------------------------------------------------"
 echo "MESHING"
 echo "---------------------------------------------------------------"
-find * -regextype posix-extended -regex '.*\.(geo)' | xargs wc -l
+git ls-files --exclude ./data/* | xargs wc -l
