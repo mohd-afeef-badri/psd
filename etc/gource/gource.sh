acec7763d43cecc50f5ac7d02800790de8d8f996
Afeef <afeef.badri@gmail.com>
2022-02-28 11:44:32 +0100

    more scales of logo

--- etc/gource/gource.sh
+++ etc/gource/gource.sh
@@ -7,6 +7,7 @@ done < name_replacements.txt)
 
 #gource --output-custom-log repo-activity.txt
 
-#gource repo-activity.txt --hide dirnames,filenames --seconds-per-day 0.03 --auto-skip-seconds 1 -1280x720 --key --title '3 years of PSD solver devlopment' --logo ./Logo/logo_trans_25pct.png --font-size 24  -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 gource.mp4
+#gource repo-activity.txt --hide dirnames,filenames --seconds-per-day 0.016 --auto-skip-seconds 1 -1280x720 --key --title '3 years of PSD solver' --logo ./../logo/logo_trans_10pct.png --font-size 30 --user-font-size 24 --highlight-users --file-filter '.*png*.' --file-filter '.*pdf*.'  --date-format "%b / %Y"  -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 gource.mp4
 
-gource repo-activity.txt --hide dirnames,filenames --seconds-per-day 0.03 --auto-skip-seconds 1 -1280x720 --key --title '3 years of PSD solver devlopment' --logo ./../logo/logo_trans_25pct.png --font-size 24
+
+gource repo-activity.txt --hide dirnames,filenames --seconds-per-day 0.016 --auto-skip-seconds 1 -1280x720 --key --title '3 years of PSD solver devlopment' --logo ./../logo/logo_trans_25pct.png --font-size 30 --user-font-size 24 --highlight-users --file-filter '.*png*.' --file-filter '.*pdf*.'  --date-format "%b / %Y"
