c2c16d6ea66f6889bc6d66e0fb23f7d3677a5e12
FARES Reine <rf258275@login01.cm.cluster>
2022-03-09 14:22:42 +0100

    some changes is the variables and the number of springs to 34

--- src/psd-preprocessor/Validation/Iwan.hpp
+++ src/psd-preprocessor/Validation/Iwan.hpp
@@ -74,27 +74,38 @@ Isv0[] = 0;
 
 string MaterialProperty = "YoungModulus PoissonRatio NumberofSpring";
 
-string YielsurfacestressCiNames1 = " YielsurfacestressCi[0] YielsurfacestressCi[1] YielsurfacestressCi[2] YielsurfacestressCi[3] YielsurfacestressCi[4] YielsurfacestressCi[5] YielsurfacestressCi[6] YielsurfacestressCi[7] YielsurfacestressCi[8] YielsurfacestressCi[9]";
-string YielsurfacestressCiNames2 = " YielsurfacestressCi[10] YielsurfacestressCi[11] YielsurfacestressCi[12] YielsurfacestressCi[13] YielsurfacestressCi[14] YielsurfacestressCi[15]";
+string YielsurfacestrainCiNames1 = " YielsurfacestrainCi[0] YielsurfacestrainCi[1] YielsurfacestrainCi[2] YielsurfacestrainCi[3] YielsurfacestrainCi[4] YielsurfacestrainCi[5] YielsurfacestrainCi[6] YielsurfacestrainCi[7] YielsurfacestrainCi[8] YielsurfacestrainCi[9]";
+string YielsurfacestrainCiNames2 = " YielsurfacestrainCi[10] YielsurfacestrainCi[11] YielsurfacestrainCi[12] YielsurfacestrainCi[13] YielsurfacestrainCi[14] YielsurfacestrainCi[15] YielsurfacestrainCi[16] YielsurfacestrainCi[17] YielsurfacestrainCi[18] YielsurfacestrainCi[19]";
+string YielsurfacestrainCiNames3 = " YielsurfacestrainCi[20] YielsurfacestrainCi[21] YielsurfacestrainCi[22] YielsurfacestrainCi[23] YielsurfacestrainCi[24] YielsurfacestrainCi[25] YielsurfacestrainCi[26] YielsurfacestrainCi[27] YielsurfacestrainCi[28] YielsurfacestrainCi[29]";
+string YielsurfacestrainCiNames4 = " YielsurfacestrainCi[30] YielsurfacestrainCi[31] YielsurfacestrainCi[32] YielsurfacestrainCi[33]";
 
-string YielsurfacestrainYiNames1 = " YielsurfacestrainYi[0] YielsurfacestrainYi[1] YielsurfacestrainYi[2] YielsurfacestrainYi[3] YielsurfacestrainYi[4] YielsurfacestrainYi[5] YielsurfacestrainYi[6] YielsurfacestrainYi[7] YielsurfacestrainYi[8] YielsurfacestrainYi[9]";
-string YielsurfacestrainYiNames2 = " YielsurfacestrainYi[10] YielsurfacestrainYi[11] YielsurfacestrainYi[12] YielsurfacestrainYi[13] YielsurfacestrainYi[14] YielsurfacestrainYi[15]";
-   
 
-string PropertyNames = MaterialProperty + YielsurfacestressCiNames1 + YielsurfacestressCiNames2 + YielsurfacestrainYiNames1 + YielsurfacestrainYiNames2;
+string YielsurfacestressYiNames1 = " YielsurfacestressYi[0] YielsurfacestressYi[1] YielsurfacestressYi[2] YielsurfacestressYi[3] YielsurfacestressYi[4] YielsurfacestressYi[5] YielsurfacestressYi[6] YielsurfacestressYi[7] YielsurfacestressYi[8] YielsurfacestressYi[9]";
+string YielsurfacestressYiNames2 = " YielsurfacestressYi[10] YielsurfacestressYi[11] YielsurfacestressYi[12] YielsurfacestressYi[13] YielsurfacestressYi[14] YielsurfacestressYi[15] YielsurfacestressYi[16] YielsurfacestressYi[17] YielsurfacestressYi[18] YielsurfacestressYi[19]";
+string YielsurfacestressYiNames3 = " YielsurfacestressYi[20] YielsurfacestressYi[21] YielsurfacestressYi[22] YielsurfacestressYi[23] YielsurfacestressYi[24] YielsurfacestressYi[25] YielsurfacestressYi[26] YielsurfacestressYi[27] YielsurfacestressYi[28] YielsurfacestressYi[29]";
+string YielsurfacestressYiNames4 = " YielsurfacestressYi[30] YielsurfacestressYi[31] YielsurfacestressYi[32] YielsurfacestressYi[33]";  
+
+string PropertyNames = MaterialProperty + YielsurfacestrainCiNames1 + YielsurfacestrainCiNames2 + YielsurfacestrainCiNames3 + YielsurfacestrainCiNames 4
+YielsurfacestressYiNames1 + YielsurfacestressYiNames2 + YielsurfacestressYiNames3 + YielsurfacestressYiNames4;
 
 
  
 real[int] PropertyValues =[
 
-	614930000.0, 0.371, 16,
-	422442500.2 , 29108930828.0 , 22717782323.0 , 18545244902.0 , 6196408158.0 , 
-	3943563406.0 , 2137231653.0 , 2142487666.0 , 909817671.5 , 432948424.3 , 
-	256315552.3 , 67747318.24 , 51552950.89 , 20695768.73 , 4435263.381 , 12587863.55,
+	614930000.0, 0.371, 34,
+	1.45281258E+10, 5.82178617E+10, 4.54355646E+10, 3.70904898E+10, 2.77751572E+10, 2.15706482E+10, 
+	1.53920096E+10, 1.23928163E+10, 1.00167542E+10, 7.88712681E+09, 6.17841185E+09, 5.25405456E+09, 
+	4.27446331E+09, 4.01462717E+09, 4.28497533E+09, 3.47094829E+09, 2.39276478E+09, 1.81963534E+09, 
+	1.42613657E+09, 1.08674829E+09, 8.65896849E+08, 7.59030876E+08, 5.12631105E+08, 3.82296058E+08, 
+	2.91042980E+08, 1.35494636E+08, 9.52184677E+07, 1.03105902E+08, 7.05953646E+07, 4.13915375E+07, 
+	2.51757271E+07, 1.47582922E+07, 8.87052676E+06, 5.95328485E+06,
 	
-	340005.5091, 420308.5133, 524405.5834, 653604.7237, 1647053.671, 2520327.797,
-	4384366.063, 5203073.539, 8131982.302, 11264333.18, 13251691.55, 16720370.49,
-	19253276.75, 20620902.16, 22768742.08, 21428992.9
+	3.40005509E+05, 4.20308513E+05, 5.24405583E+05, 6.53604724E+05, 8.05746026E+05, 1.02172957E+06, 
+ 	1.26789373E+06, 1.64705367E+06, 2.01271770E+06, 2.52032780E+06, 3.05546227E+06, 3.75162146E+06, 
+ 	4.38436606E+06, 5.22502629E+06, 5.80307354E+06, 6.42397205E+06, 7.14120247E+06, 8.13198230E+06, 
+	9.07056705E+06, 1.01794843E+07, 1.12643332E+07, 1.23695744E+07, 1.32516915E+07, 1.46063819E+07, 
+	1.55155396E+07, 1.67173705E+07, 1.84181142E+07, 1.92532768E+07, 1.98852369E+07, 2.06809022E+07, 
+	2.14289929E+07, 2.21412448E+07, 2.27687421E+07, 2.32772260E+07
 
 ];
 
