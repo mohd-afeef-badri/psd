bba7c442d77f60ebe71a6895afd426e578093bbb
Mohd Afeef BADRI <afeef.badri@gmail.com>
2025-07-14 20:04:06 +0200

    correct copyright

--- src/psd-preprocessor/ControlParameters/poisson.hpp
+++ src/psd-preprocessor/ControlParameters/poisson.hpp
@@ -1,9 +1,25 @@
 /**************************************************************************************
 *                                                                                     *
-* Author : Mohd Afeef BADRI, Rania SAADI                                              *
-* Email  : mohd-afeef.badri@cea.fr, rania.saadi@cea.fr                                *
-* Date   : 15/10/2024                                                                 *
-* Type   : Support file                                                               *
+*           This file is a part of PSD (Parallel Structural Dynamics)                 *
+*                                                                                     *
+*       -------------------------------------------------------------------           *
+*                                                                                     *
+*       Copyright 2019-2025 CEA/DES                                                   *
+*                                                                                     *
+*       Licensed under the Apache License, Version 2.0  (the "License");              *
+*       you may not use this file except in compliance with the License.              *
+*       You may obtain a copy of the License at                                       *
+*                                                                                     *
+*           http://www.apache.org/licenses/LICENSE-2.0                                *
+*                                                                                     *
+*       Unless required by applicable law or agreed to in writing, software           *
+*       distributed under the License is distributed on an  "AS IS"  BASIS,           *
+*       WITHOUT  WARRANTIES  OR  CONDITIONS  OF  ANY  KIND,  either express           *
+*       or implied. See  the License  for  the  specific language governing           *
+*       permissions and limitations under the License.                                *
+*                                                                                     *
+*       -------------------------------------------------------------------           *
+*                                                                                     *
 *                                                                                     *
 * Comment: This support  file is  responsible for generating  ControlParameters.edp   *
 *          which contain main solver control parameters of PSD.                       *
